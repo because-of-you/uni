a47785b873ff9d4c46f801fce773cd95fa749def
wfy-belief <mail@acitrus.cn>
2023-01-10 14:53:09 +0800

    feat: 增加监控示例

---
+++ bean/src/main/java/cn/acitrus/uni/datasource/UniDataSourceMeterRegistry.java
@@ -0,0 +1,43 @@
+package cn.acitrus.uni.datasource;
+
+import io.micrometer.common.util.internal.logging.InternalLogger;
+import io.micrometer.common.util.internal.logging.InternalLoggerFactory;
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
+import io.micrometer.core.instrument.step.StepMeterRegistry;
+import io.micrometer.core.instrument.step.StepRegistryConfig;
+import io.micrometer.core.instrument.util.NamedThreadFactory;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * {@code @author:} wfy
+ * {@code @date:} 2023/1/10
+ **/
+@Slf4j
+public class UniDataSourceMeterRegistry extends StepMeterRegistry {
+
+    public UniDataSourceMeterRegistry() {
+        this(UniDataSourceRegistryConfig.DEFAULT, Clock.SYSTEM);
+    }
+
+    public UniDataSourceMeterRegistry(StepRegistryConfig config, Clock clock) {
+        super(config, clock);
+        start(new NamedThreadFactory("uni-datasource-metrics-publisher"));
+    }
+
+    @Override
+    protected void publish() {
+        List<Meter> meters = getMeters().stream().toList();
+        log.info("{}", meters);
+    }
+
+    @Override
+    protected TimeUnit getBaseTimeUnit() {
+        return TimeUnit.MILLISECONDS;
+    }
+}
