d46db64d1c7adc35663cd8a5f490c0a06c7a7bd1
wfy <wfy@acitrus.cn>
2023-01-10 08:27:05 +0800

    feat: 移动位置

---
+++ server/src/main/java/cn/acitrus/uni/server/socket/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package cn.acitrus.uni.server.socket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * {@code @author:} wfy
+ * {@code @date:} 2023/1/3
+ **/
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
