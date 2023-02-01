87a8a9f2548258a27c1e9f21ed55e37f94991372
wfy-belief <mail@acitrus.cn>
2023-02-01 17:37:48 +0800

    add: 增加线程池配置

--- bean/src/main/java/cn/acitrus/uni/bean/auditing/AuditorAware.java
+++ bean/src/main/java/cn/acitrus/uni/bean/auditing/AuditorAware.java
@@ -1,7 +1,6 @@
 package cn.acitrus.uni.bean.auditing;
 
 import lombok.NonNull;
-import org.springframework.data.domain.AuditorAware;
 
 import java.util.Optional;
 
@@ -9,7 +8,7 @@ import java.util.Optional;
  * {@code @author:} wfy
  * {@code @date:} 2023/1/16
  **/
-public class UniAuditorAware implements AuditorAware<String> {
+public class AuditorAware implements org.springframework.data.domain.AuditorAware<String> {
     @Override
     @NonNull
     public Optional<String> getCurrentAuditor() {
