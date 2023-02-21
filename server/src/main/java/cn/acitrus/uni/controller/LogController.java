66caea4fbc015c24a7508e9aed1e4705d95e9eed
wfy-belief <mail@acitrus.cn>
2023-02-21 16:19:24 +0800

    add: 日志采集

--- server/src/main/java/cn/acitrus/uni/controller/LogController.java
+++ server/src/main/java/cn/acitrus/uni/controller/LogController.java
@@ -25,4 +25,13 @@ public class LogController {
         hashMap.put("code", 0);
         return Mono.just(hashMap);
     }
+
+    @PostMapping("services/collector/event/1.0")
+    public Mono<HashMap<Object, Object>> test1(@RequestBody Map<String, Objects> map) {
+        log.info("日志采集触发 -> {}", map);
+        HashMap<Object, Object> hashMap = new HashMap<>();
+        hashMap.put("text", "Success");
+        hashMap.put("code", 0);
+        return Mono.just(hashMap);
+    }
 }
