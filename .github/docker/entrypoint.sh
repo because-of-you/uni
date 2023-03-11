6bee402dd9090f25553cb19917e69ebd30d9907c
wfy-belief <mail@acitrus.cn>
2023-03-11 11:17:21 +0800

    change: 使用open j9

--- .github/docker/entrypoint.sh
+++ .github/docker/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 #./builder
-java -XX:+UseContainerSupport -XX:InitialRAMPercentage=70.0 -XX:MaxRAMPercentage=70.0 -jar server/target/server-exec.jar
+java \
+  -XX:+UseContainerSupport \
+  -XX:InitialRAMPercentage=30.0 \
+  -XX:MaxRAMPercentage=30.0 \
+  -Xshareclasses:cacheDir=.cache/javasharedresources \
+  -Xtune:virtualized \
+  -Xquickstart \
+  -jar server/target/server-exec.jar
