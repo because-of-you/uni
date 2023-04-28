ce49332e6f4c0f997b6319c1039c0f53105c728b
wfy-belief <mail@acitrus.cn>
2023-04-28 17:12:24 +0800

    add: 完善上下文信息

---
+++ common/src/main/java/cn/acitrus/common/entities/permissions/TokenContext.java
@@ -0,0 +1,11 @@
+package cn.acitrus.common.entities.permissions;
+
+import lombok.Data;
+
+/**
+ * {@code @author:} wfy <br/>
+ * {@code @date:} 2023/4/28
+ **/
+@Data
+public class TokenContext {
+}
---
+++ server/src/main/java/cn/acitrus/server/config/AuditorAwareConfig.java
@@ -0,0 +1,28 @@
+package cn.acitrus.server.config;
+
+import jakarta.annotation.Nonnull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+/**
+ * {@code @author:} wfy <br/>
+ * {@code @date:} 2023/4/28
+ **/
+@Configuration
+public class AuditorAwareConfig {
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new UniAuditorAware();
+    }
+
+    public static class UniAuditorAware implements AuditorAware<String> {
+        @Nonnull
+        @Override
+        public Optional<String> getCurrentAuditor() {
+            return Optional.of("uni");
+        }
+    }
+}
